d315945406bd99e6241d478340b327cce396f6b3
Hannah-Scheuer <hscheuer20@gmail.com>
2021-01-12 15:41:43 -0500

    set up the file

---
+++ Radix.java
@@ -0,0 +1,11 @@
+public class Radix{
+
+  //get nth digit of an int, where 0 is the ones column, 1 is the tens column etc.
+  public static int nth(int n, int col){}
+
+  //return the number of digits in n.
+  public static int length(int n){}
+
+  //Merge all of the linked lists in the bucket array into your original linked list. The original may have elements, and those should be kept.
+  public static void merge(MyLinkedList original,MyLinkedList[]buckets){}
+}
