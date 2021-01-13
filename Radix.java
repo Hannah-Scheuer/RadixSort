d2a2aaa800c358b738a30fa802e4564d1d437403
Hannah-Scheuer <hscheuer20@gmail.com>
2021-01-13 07:12:18 -0500

    draft of radix sort

--- Radix.java
+++ Radix.java
@@ -16,12 +16,36 @@ public class Radix{
   //Merge all of the linked lists in the bucket array into your original linked list. The original may have elements, and those should be kept.
   public static void merge(SortableLinkedList original, SortableLinkedList[]buckets){
     for (int i = 0; i<10;i++){
-      original.extend([i]buckets);
+      original.extend(buckets[i]);
     }
   }
 
   public static void radixSortSimple(SortableLinkedList data){
-    
+    int maxLength = 0;
+    for (int i = 0; i < data.size(); i++) {
+      if (data.get(i).length() > maxLength){
+      maxLength = data.get(i).length();
+      }
+    }
+
+    for (int i=0;i<maxLength;i++){
+      new SortableLinkedList [] buckets = new SortableLinkedList[10];
+      for (int i = 0; i< buckets.length; i++){
+        new SortableLinkedList b = new SortableLinkedList();
+        buckets[i] = b;
+      }
+      for (int j = 0; j < data.size(); j++){
+        int digit = nth(data.get(j), i);
+        buckets[digit].add(j);
+      }
+      new SortableLinkedList sorted = new SortableLinkedList();
+      Radix.merge(sorted, buckets);
+
+      new SortableLinkedList temp = new SortableLinkedList();
+      temp.extend(data);
+
+      data.extend(sorted);
+    }
   }
 
 }
